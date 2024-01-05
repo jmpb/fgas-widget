e2c1ea9718689068257a48baabb3cc9950e3c0bc
James Baker <james.b.github@ffdgroup.co.uk>
2024-01-05 10:17:39 +0000

    Refactored JS functions to fetch FGAS module records

--- app/js/extension.js
+++ app/js/extension.js
@@ -24,34 +24,71 @@ function getSONumber() {
     });
 }
 
-function getFGASRecord(module_api_name, url_query, compare_property, store_key) {
+function getFGASRecord() {
     return new Promise((resolve, reject) => {
+        
+        let so_number = Alpine.store('salesorder_number');
+        let fgas_record_name = so_number.replace("SO", "FGAS");
+
         // fetch the FGAS data for the salesorder from the custom module
         var options = {
-            url: 'https://inventory.zoho.eu/api/v1/' + module_api_name + '/',
+            url: 'https://inventory.zoho.eu/api/v1/cm_fgas_record/',
             method: "GET",
             url_query: [
             {
                 key: 'organization_id',
                 value: '20067754174'
+            },
+            {
+                key:"cf_record_id",
+                value: fgas_record_name
             }
             ],
             connection_link_name: 'inv_conn'
         };
 
-        options.url_query.push(url_query);
-
         ZFAPPS.request(options).then(function (response) {
             let responseJSON = JSON.parse(response.data.body);
-            let so_number = Alpine.store('salesorder_number');
-            let fgas_record_name = so_number.replace("SO", "FGAS");
             for (let index = 0; index < responseJSON.module_records.length; index++) {
                 const element = responseJSON.module_records[index];
-                if (element.compare_property == fgas_record_name) {
-                    Alpine.store(store_key, element);
+                if (element.record_name == fgas_record_name) {
+                    Alpine.store("fgasrecord", element);
                     resolve();
                 }
             };
+        }).catch(function (err) {
+            reject(err);
+        });
+    })
+}
+
+function getFGASRelatedRecord(module_api_name, store_key) {
+    return new Promise((resolve, reject) => {
+
+        let so_number = Alpine.store('salesorder_number');
+        let fgas_record_name = so_number.replace("SO", "FGAS");
+
+        // fetch the FGAS related data for the salesorder from the custom module
+        var options = {
+            url: 'https://inventory.zoho.eu/api/v1/' + module_api_name +'/',
+            method: "GET",
+            url_query: [
+            {
+                key: 'organization_id',
+                value: '20067754174'
+            },
+            {
+                key:"cf_fgas_record",
+                value: fgas_record_name
+            }
+            ],
+            connection_link_name: 'inv_conn'
+        };
+
+        ZFAPPS.request(options).then(function (response) {
+            let responseJSON = JSON.parse(response.data.body);
+            Alpine.store(store_key, responseJSON.module_records);
+            resolve();
         }).catch(function (err) {
             reject(err);
         });
